26c686772303de5ae85cf965197133bdb5263ccd
Evyatar Rosner <evyatar.rosner@tikalk.com>
2021-01-06 16:50:08 +0200

    update API url

--- src/Weather/Weather.js
+++ src/Weather/Weather.js
@@ -8,7 +8,7 @@ class Weather extends Component {
 			<div className="Weather">
 				<p>
 					Use the following API to display the hottest and coldest days:<br />
-					<code>https://my-json-server.typicode.com/evyros/react-weather-app/forecast</code>
+					<code>https://netcraft2.s3-eu-west-1.amazonaws.com/weather.json</code>
 				</p>
 				<div className="Weather__stats">
 					<strong>Hottest day is:</strong>
